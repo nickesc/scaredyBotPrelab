6925f2ed090892e82405a4687927e035a46f4957
N. Escobar <nickesc.media@gmail.com>
2022-05-02 16:31:22 -0700

    sensor tests w new sensors

--- sensors.py
+++ sensors.py
@@ -3,16 +3,39 @@
 import RPi.GPIO as GPIO
 import time
 
-ObstaclePin = 17
-pirPin = 27  # the pir connect to pin17
+obstaclePin = 17 # obstacle module pin = GPIO17 (BCM) / 11 (board)
+pirPin = 27  # PIR motion pin = GPIO27 (BCM) / 13 (board)
+trigPin = 23 # ultrasonic module trig pin = GPIO23 (BCM) / 16 (board)
+echoPin = 24 # ultrasonic module echo pin = GPIO24 (BCM) / 18 (board)
+
 
 
 def setup():
     GPIO.setmode(GPIO.BCM)  # Set the GPIO modes to BCM Numbering
-    GPIO.setup(pirPin, GPIO.IN)  # Set pirPin to input
-    GPIO.setup(ObstaclePin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+    GPIO.setup(pirPin, GPIO.IN)
+    GPIO.setup(obstaclePin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+    GPIO.setup(trigPin, GPIO.OUT)
+    GPIO.setup(echoPin, GPIO.IN)
 
 
+def distance():
+    GPIO.output(trigPin, 0)
+    time.sleep(0.000002)
+
+    GPIO.output(trigPin, 1)
+    time.sleep(0.00001)
+    GPIO.output(trigPin, 0)
+
+    while GPIO.input(echoPin) == 0:
+        a = 0
+    time1 = time.time()
+    while GPIO.input(echoPin) == 1:
+        a = 1
+    time2 = time.time()
+
+    during = time2 - time1
+    return during * 340 / 2 * 100
 
 def MAP(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
@@ -22,12 +45,14 @@ def MAP(x, in_min, in_max, out_min, out_max):
 def loop():
     while True:
 
-        pir_val = GPIO.input(pirPin)
-        print(pir_val)
+        dis = distance()
+        print('Distance: %.2f' % dis)
 
-        if (0 == GPIO.input(ObstaclePin)):
-            print("Detected Barrier!")
-            time.sleep(1)
+        pir_val = GPIO.input(pirPin)
+        print("Motion:",pir_val)
+
+        print("Barrier:", GPIO.input(obstaclePin))
+        time.sleep(.02)
 
 
 
