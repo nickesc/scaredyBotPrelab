242ed11f6f272787df94d09a8e6b46bb69ff01da
N. Escobar <nickesc.media@gmail.com>
2022-05-03 00:06:09 -0700

    add get sensor method

--- oldSensors.py
+++ oldSensors.py
@@ -20,13 +20,8 @@ phaseLog = open(phaseLogName, 'a+')
 outLog = open(outLogName, 'a+')
 writer = csv.DictWriter(outLog,fieldnames = fields)
 
-
 currPhase = ['']
 phases = []
-
-motion=[0]*(100)
-motionIndex = 0
-
 
 def logPhase(phase, full = False):
     currPhase[0]=phase
@@ -55,52 +50,7 @@ def setup():
     GPIO.setup(trigBack, GPIO.OUT)
     GPIO.setup(echoBack, GPIO.IN)
 
-def getBack():
-    logPhase('distBack')
-    global trigBack, echoBack  # Allow access to 'trig' and 'echo' constants
-
-    if GPIO.input(echoBack):  # If the 'Echo' pin is already high
-        return (100)  # then exit with 100 (sensor fault)
-
-    distance = 0  # Set initial distance to zero
-
-    GPIO.output(trigBack, False)  # Ensure the 'Trig' pin is low for at
-    time.sleep(0.05)  # least 50mS (recommended re-sample time)
-
-    GPIO.output(trigBack, True)  # Turn on the 'Trig' pin for 10uS (ish!)
-    dummy_variable = 0  # No need to use the 'time' module here,
-    dummy_variable = 0  # a couple of 'dummy' statements will do fine
-
-    GPIO.output(trigBack, False)  # Turn off the 'Trig' pin
-    time1, time2 = time.time(), time.time()  # Set inital time values to current time
-
-    while not GPIO.input(echoBack):  # Wait for the start of the 'Echo' pulse
-        time1 = time.time()  # Get the time the 'Echo' pin goes high
-        if time1 - time2 > 0.02:  # If the 'Echo' pin doesn't go high after 20mS
-            distance = 100  # then set 'distance' to 100
-            break  # and break out of the loop
-
-    if distance == 100:  # If a sensor error has occurred
-        return (distance)  # then exit with 100 (sensor fault)
-
-    while GPIO.input(echoBack):  # Otherwise, wait for the 'Echo' pin to go low
-        time2 = time.time()  # Get the time the 'Echo' pin goes low
-        if time2 - time1 > 0.02:  # If the 'Echo' pin doesn't go low after 20mS
-            distance = 100  # then ignore it and set 'distance' to 100
-            break  # and break out of the loop
-
-    if distance == 100:  # If a sensor error has occurred
-        return (distance)  # then exit with 100 (sensor fault)
-
-        # Sound travels at approximately 2.95uS per mm
-        # and the reflected sound has travelled twice
-        # the distance we need to measure (sound out,
-        # bounced off object, sound returned)
-
-    distance = (time2 - time1) / 0.00000295 / 2 / 10  # Convert the timer values into centimetres
-    return (distance)  # Exit with the distance in centimetres
-
-def getFront(sensor):
+def getDist(sensor):
     logPhase('distFront')
     global trigFront, echoFront, trigBack, echoBack  # Allow access to 'trig' and 'echo' constants
 
@@ -193,20 +143,16 @@ def getFront(sensor):
     return format(distance,'.2f')  # Exit with the distance in centimetres
 
 def getMotion(maxSample = 10):
-    global motion
-
     logPhase('motion')
 
-    motion.pop()
-    motion = [GPIO.input(pirPin)] + motion
-
-    if (motion[:maxSample-1].count(0) > maxSample/2):
-        return [False, GPIO.input(pirPin)]
+    if (GPIO.input(pirPin) == 0):
+        return False
     else:
-        return [True, GPIO.input(pirPin)]
+        return True
 
 def getObstacle():
     logPhase('obstacle')
+
     if (GPIO.input(obstaclePin) == 1):
         return False
     else:
@@ -215,31 +161,31 @@ def getObstacle():
 def MAP(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
+def getSensors(output = False):
+    distFront = getDist('front')
+    distBack = getDist('back')
+
+    motion = getMotion()
+
+    obstacle = getObstacle()
+    logPhase('collectedData')
+
+    data = {'disFront': distFront, 'disBack': distBack, 'pirVal': motion, 'obstacle': obstacle}
+
+    logPhase('wroteData')
+    writer.writerow(data)
+
+    if(output):
+        logPhase('wroteData')
+        print("{: >10} {: >10} {: >10} {: >10}".format(*[distFront, distBack, motion, obstacle]))
+
+    return(data)
 
 def loop():
     logPhase('loopStart')
-    print("{: >10} {: >10} {: >10} {: >10} {:>10}".format(*["distFront", "distBack", "motionRaw", "motionAverage", "obstacle"]))
     while True:
-        distFront = getFront('front')
-        distBack = getFront('back')
-
-        mot = getMotion()
-
-        motionAverage = mot[0]
-        motionRaw = mot[1]
-
-        obstacle = getObstacle()
-        logPhase('collectedData')
-
-        data = {'disFront': distFront, 'disBack': distBack, 'pirVal': motionRaw, 'obstacle': obstacle}
-
-        writer.writerow(data)
-
-        logPhase('wroteData')
-
-        print("{: >10} {: >10} {: >10} {: >10} {:>10}".format(*[distFront,distBack,motionRaw,motionAverage,obstacle]))
-
-        #print(data, motionAverage)
+        getSensors()
+        time.sleep(.3)
 
 
 def destroy():
